37582d19af84334d506bfb0be2e29662d37bcc88
Umer-Islam <139012668+Umer-Islam@users.noreply.github.com>
2024-02-06 21:19:14 +0500

    add ui to rockPaperScissor game

--- main.js
+++ main.js
@@ -1,44 +1,56 @@
+const choices = ["rock", "paper", "scissors"];
+const player = document.getElementById("player");
+const computer = document.getElementById("computer");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
 
-// This function makes a random choice.
-let getComputerChoice = () => {
-  let choose = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
-  let choice = Math.floor(Math.random() * 3);
-  return choose[choice];
-};
-// below variables are declared for use in functions later
-let computerSelection = getComputerChoice();
-let playerSelection = prompt('ROCK,Paper,or Scissors..... DECIDE')
-playerSelection = playerSelection.toLowerCase();
-// just for testing.
-console.log({ computerSelection });
-console.log({ playerSelection });
+function playGame(choice) {
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let result = "";
+  if (choice === computerChoice) {
+    result = "Draw";
+  } else {
+    switch (choice) {
+      case "rock":
+        result = computerChoice === "scissors" ? "you win" : "you lose";
+        break;
+      case "paper":
+        result = computerChoice === "rock" ? "you win" : "you lose";
+        break;
 
-// This function takes choice of user and computer and decides who won or is it a draw
-function round (playerSelection, computerSelection) {
-   
-    if (computerSelection !== playerSelection) {
-    if (
-      (playerSelection === "rock" && computerSelection === "scissors") ||
-      (playerSelection === "scissors" && computerSelection === "rock")
-    ) {
-      return playerSelection === "rock" ? "you won" : "Computer wins";
+      case "scissors":
+      result = computerChoice === "paper" ? "you win" : "you lose";
+      break;
+
     }
-     else if (
-      (playerSelection === "scissors" && computerSelection === "paper") ||
-      (playerSelection === "paper" && computerSelection === "scissors")
-    ) {
-      return playerSelection === "scissors" ? "you won" : "Computer wins";
-    } else if (
-      (playerSelection === "paper" && computerSelection === "rock") ||
-      (playerSelection === "rock" && computerSelection === "paper")
-    ) {
-      return playerSelection === "paper" ? "you won" : "Computer wins";
-    }
-  }else{
-    console.log('draw')
-    return `Draw`
   }
-};
-//playing a round
-console.log("result of the round: ", round(playerSelection,computerSelection));
+
+  player.textContent = `Player: ${choice}`;
+  computer.textContent = `Computer: ${computerChoice}`;
+  resultDisplay.textContent = `${result}`;
+
+  switch (result) {
+    case "you win":
+      document.getElementById("playerScoreDisplay").style.color = 'green'
+      playerScore++;
+      playerScoreDisplay.textContent = playerScore;
+      break;
+    case "you lose":
+      document.getElementById("computerScoreDisplay").style.color = 'red'
+      
+      computerScore++;
+      computerScoreDisplay.textContent = computerScore;
+      break;
+  }
+  if(result ==='you win'){
+    resultDisplay.style.color= 'green'
+  }else if(result ==='you lose'){
+    resultDisplay.style.color= 'red'}
+    else{
+      resultDisplay.style.color= 'black'
+    }
+}
